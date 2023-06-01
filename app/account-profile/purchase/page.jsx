e1259e1b3f1ba44f1ffcd369afb71f4f22bad7b7
the-gaurav-bhatt <bhattgaurav654@gmail.com>
2023-06-01 10:31:30 +0545

    jhjhjhh

--- app/account-profile/purchase/page.jsx
+++ app/account-profile/purchase/page.jsx
@@ -1,25 +1,25 @@
-'use client';
+// 'use client';
 
 import CheckoutSummary from '@/app/components/checkoutSection/CourseSummary';
 import CheckoutPage1 from '@/app/components/checkoutSection/Payment1';
-import { userContext } from '@/app/layout';
-import { useRouter } from 'next/navigation';
-import React, { useContext } from 'react';
+// import { userContext } from '@/app/layout';
+// import { useRouter } from 'next/navigation';
+import React from 'react';
 
 const Page = () => {
-  const router = useRouter();
-  const { user } = useContext(userContext);
-  if (!user._id) {
-    router.push('/login');
-  } else {
-    return (
-      <div className="  ">
-        <CheckoutSummary />
-        {/* <CheckoutForm />{' '} */}
-        <CheckoutPage1 />
-      </div>
-    );
-  }
+  // const router = useRouter();
+  // const { user } = useContext(userContext);
+  // if (!user._id) {
+  //   router.push('/login');
+  // } else {
+  return (
+    <div className="  ">
+      <CheckoutSummary />
+      {/* <CheckoutForm />{' '} */}
+      <CheckoutPage1 />
+    </div>
+  );
+  // }
 };
 
 export default Page;
