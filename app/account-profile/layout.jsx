be383c36198a4032f02a9471ab7de75afd099751
the-gaurav-bhatt <bhattgaurav654@gmail.com>
2023-06-01 09:58:43 +0545

    layout error fixed

--- app/account-profile/layout.jsx
+++ app/account-profile/layout.jsx
@@ -1,13 +1,6 @@
-'use client';
 import React from 'react';
 import SideBar from '../components/profileSection/SideBar';
-import { useState } from 'react';
 const layout = ({ children }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  // const toggleMenu = () => {
-  //   setIsOpen(!isOpen);
-  // };
-
   return (
     <section className="flex  ">
       <SideBar />
